1ae039b70e883992e94431c5cab28d6cd324787b
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-03-15 11:50:21 -0400

    Trying to create new branch

---
+++ Mixr.Party.Maui/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Mixr.Party.Maui
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ Mixr.Party.Maui/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Mixr.Party.Maui
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass1
+    {
+    }
+}
